6c42d332f15b7eadddbb4f6dd5c5be1ca2d52fa2
Alex P <alexeek@protonmail.com>
2020-07-21 12:47:47 +0100

    refactor: rename routes package to handlers

--- core/web/routes.go
+++ core/web/routes.go
@@ -1,12 +1,12 @@
 package web
 
 import (
-	"github.com/fjah/seachad/core/web/routes"
+	"github.com/fjah/seachad/core/web/handlers"
 )
 
 func (s *Server) setRoutes() {
 	v1 := s.engine.Group("/api/v1")
 	{
-		v1.GET("/test", routes.Test)
+		v1.GET("/test", handlers.Test)
 	}
 }
